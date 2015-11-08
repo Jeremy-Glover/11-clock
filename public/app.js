e4334ef2cc669cde98df8340afa2c5166f571650
Jeremy Glover <jeremyglover33@gmail.com>
2015-11-08 11:51:25 -0600

    Padded zeros, modified, styled progress bar

--- public/app.js
+++ public/app.js
@@ -1,12 +1,29 @@
 (function() {
-  var time = document.querySelector('.time');
 
+  var time = document.querySelector('.time');
+  var line = document.querySelector('.line');
 
   window.setInterval(() => {
     var d = new Date();
     console.log(d);
-    var now = `${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`
+    var nowHour = padZeros(d.getHours());
+    var nowMin =  padZeros(d.getMinutes());
+    var nowSec =  padZeros(d.getSeconds());
+    var now = `${nowHour}:${nowMin}:${nowSec}`;
     time.innerHTML = now;
+    console.log(nowSec / 60);
+    line.style.width = (nowSec / 60 * 40 + 'rem');
+
   }, 1000);
+
+  function padZeros(time) {
+    if (time < 10) {
+      return '0' + time;
+    }
+
+    return time;
+  }
+
+
 
 })();
