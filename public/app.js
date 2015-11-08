8f58033682375789dc6a4813f55fd3e77d57770f
Jeremy Glover <jeremyglover33@gmail.com>
2015-11-08 13:20:27 -0600

    Updated background color of page

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 (function() {
-
   var time = document.querySelector('.time');
   var line = document.querySelector('.line');
+  var colorContainer = document.querySelector('.container');
 
   window.setInterval(() => {
     var d = new Date();
@@ -11,10 +11,19 @@
     var nowSec =  padZeros(d.getSeconds());
     var now = `${nowHour}:${nowMin}:${nowSec}`;
     time.innerHTML = now;
-    console.log(nowSec / 60);
-    line.style.width = (nowSec / 60 * 40 + 'rem');
 
+    var colorHour = toColorRange(nowHour, 60);
+    var colorMin = toColorRange(nowMin, 60);
+    var colorSec = toColorRange(nowSec, 60);
+    var colorApply = `rgb(${colorHour}, ${colorMin}, ${colorSec})`;
+    colorContainer.style.background = colorApply;
+
+    line.style.width = (nowSec / 60 * 40 + 'rem');
   }, 1000);
+
+  var toColorRange = (number, base) => {
+    return Math.floor(number / base * 255);
+  };
 
   function padZeros(time) {
     if (time < 10) {
@@ -23,7 +32,5 @@
 
     return time;
   }
-
-
 
 })();
